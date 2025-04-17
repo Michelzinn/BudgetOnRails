858ad6a1b0e0eef18198f2d12b056cb17fa2f997
michelzinn <michelbreno5@hotmail.com>
2025-04-17 16:27:00 -0300

    test: creating budget_allocation factory

---
+++ spec/factories/budget_allocation.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :budget_allocation do
+    association :budget
+    association :category
+    amount_allocated { "10000" }
+    created_at { Time.now }
+    updated_at { Time.now }
+  end
+end
